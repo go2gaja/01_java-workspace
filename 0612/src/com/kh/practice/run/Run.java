0d9a26a65d7ce1cd4305c26f441cb6ca9c4fd39a
go2gaja <93joyko@gmail.com>
2023-06-16 18:27:44 +0900

    a
    a

---
+++ 0612/src/com/kh/practice/run/Run.java
@@ -0,0 +1,35 @@
+package com.kh.practice.run;
+
+import com.kh.practice.model.dao.CoffeeDao;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		
+		CoffeeDao dao = new CoffeeDao();
+		//dao.coffeeArr();
+		dao.fileSave();
+		
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
